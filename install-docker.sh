82db7e8b06cd42ac63308bbc4dbe9a1ebd646e3e
matthew-js-porter <matthew.js.porter@jpmchase.com>
2022-01-18 12:19:27 -0500

    test

--- install-docker.sh
+++ install-docker.sh
@@ -3,7 +3,8 @@ apt-get install curl -y
 update-ca-certificates --fresh
 
 # Install docker
-curl -sL get.docker.com | bash || {
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh || {
     # Fix for arm/v6 https://markmcgookin.com/2019/08/04/how-to-install-docker-on-a-raspberry-pi-zero-w-running-raspbian-buster/
     cd /tmp
     wget https://packagecloud.io/Hypriot/rpi/packages/raspbian/buster/containerd.io_1.2.6-1_armhf.deb/download.deb
